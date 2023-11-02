dc107e6ea10cdeaf19fae3d88bbe794b6b5be525
Adam <adammiller.2000nj@gmail.com>
2023-11-02 15:46:20 -0400

    added more matches

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ playerTwoProfile.addEventListener('click', () => {
     }
 })
 
-let currentNum = 30;
+let currentNum = 50;
 let nextBtn = document.getElementById('nextBtn');
 nextBtn.addEventListener('click', () => {
     randomNum = Math.floor(Math.random() * currentNum) + 1;
@@ -97,8 +97,28 @@ function matches(){
         case 25: new Match("aMSa", "Mango", "Winners Quarters", "Smash'N'Splash 5", 1); break;
         case 26: new Match("Cody Schwab", "Ginger", "Losers Semis", "The Script: Episode 2", 2); break;
         case 27: new Match("Zain", "S2J", "Winners Finals", "DPOTG 2018", 2); break;
-        case 28: new Match("Leffen", "Axe", "Losers Quarters", "Smash Summit 7", 1); break;
+        case 28: new Match("Axe", "Leffen", "Losers Quarters", "Smash Summit 7", 2); break;
         case 29: new Match("Duck", "Hungrybox", "Grand Finals - Set 1", "Canada Cup 2018", 1); break;
-        case 30: new Match("Mango", "Hungrybox", "Group 1 Pools", "Smash Summit 6", 1); break;
+        case 30: new Match("Hungrybox", "Mango", "Group 1 Pools", "Smash Summit 6", 2); break;
+        case 31: new Match("Wizzrobe", "Zain", "Losers Top 12", "Full Bloom 4", 1); break;
+        case 32: new Match("SFAT", "Lucky", "Losers Top 16", "Genesis 5", 2); break;
+        case 33: new Match("Hungrybox", "Albert", "Losers Quarters", "Low Tier City 7", 1); break;
+        case 34: new Match("Llod", "Polish", "Winners Quarters", "Super Smash Con 2019", 1); break;
+        case 35: new Match("Cody Schwab", "Fiction", "Grand Finals - Set 1", "Saving Mr. Lombardi 2", 2); break;
+        case 36: new Match("Axe", "Wizzrobe", "Group A Pools", "Smash Summit 9", 1); break;
+        case 37: new Match("Gahztu", "S2J", "Losers Eighths", "Low Tide City 2021", 1); break;
+        case 38: new Match("Hungrybox", "Cody Schwab", "Losers Semis", "SWT 2021 NA East", 2); break;
+        case 39: new Match("Jmook", "Plup", "Winners Top 32", "Genesis 8", 1); break;
+        case 40: new Match("Zain", "Llod", "Group A Pools", "Smash Summit 13", 2); break;
+        case 41: new Match("Hungrybox", "aMSa", "Winners Finals", "LVL UP EXPO 2023", 2); break;
+        case 42: new Match("aMSa", "Aklo", "Winners Finals", "The Come Up 2023", 1); break;
+        case 43: new Match("Leffen", "Plup", "Winners Quarters", "Genesis 4", 2); break;
+        case 44: new Match("Armada", "Leffen", "Winners Finals", "BEAST 7", 1); break;
+        case 45: new Match("SFAT", "PewPewU", "Losers Top 12", "Smash Summit Spring 2017", 1); break;
+        case 46: new Match("Leffen", "Duck", "Winners Quarters", "Full Bloom 3", 2); break;
+        case 47: new Match("Axe", "Mango", "Losers Eighths", "Smash Rivalries", 1); break;
+        case 48: new Match("Mew2King", "SFAT", "Winners Finals", "CEO Dreamland", 2); break;
+        case 49: new Match("Mango", "Armada", "Winners Semis", "Royal Flush", 1); break;
+        case 50: new Match("Axe", "Hungrybox", "Winners Semis", "CEO 2017", 2); break;
     }
 }
